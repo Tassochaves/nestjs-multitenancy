84faa925c6c0304cea862ad38b925c07a88419fc
Tassochaves <tassogt@hotmail.com>
2025-04-21 23:30:26 -0300

    Feat: implements the guardian and prepares partner endpoint structure.

---
+++ src/partners/partners.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { PartnersService } from './partners.service';
+import { CreatePartnerDto } from './dto/create-partner.dto';
+import { UpdatePartnerDto } from './dto/update-partner.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller('partners')
+export class PartnersController {
+  constructor(private readonly partnersService: PartnersService) {}
+
+  @Post()
+  create(@Body() createPartnerDto: CreatePartnerDto) {
+    return this.partnersService.create(createPartnerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.partnersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.partnersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
+    return this.partnersService.update(+id, updatePartnerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.partnersService.remove(+id);
+  }
+}
