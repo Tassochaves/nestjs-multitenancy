6fb297349645ce3ce519dfc4154d9b23b255ac7a
Tassochaves <tassogt@hotmail.com>
2025-04-21 21:12:41 -0300

    Feat: configure the prisma module to global and create a user.

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-  @Get()
-  create(data) {
+
+  @Post()
+  create(@Body() data: CreateUserDto) {
     return this.usersService.create(data);
   }
 }
--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRoles } from './user-roles';
 
 @Injectable()
 export class UsersService {
-  create(data) {}
+  constructor(private prismaService: PrismaService) {}
+  create(data: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        roles: [UserRoles.PARTNER],
+      },
+    });
+  }
 }
